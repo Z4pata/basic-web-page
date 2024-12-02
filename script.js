8f94ad8e42c8bca5b75159dac200005eefdc5e0c
user <>
2024-12-02 06:47:33 -0500

    Feat: creation of the page

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('celebrationButton').addEventListener('click', function() {
+    const sorpresa = document.getElementById('sorpresa');
+    sorpresa.classList.toggle('hidden');
+});
